2d9584fdc5b23067d1e2e56784be4b9dd71199da
APikielny <apikielny@gmail.com>
2020-12-01 16:49:45 -0800

    write video function

--- code/helper.py
+++ code/helper.py
@@ -20,3 +20,17 @@ def load_video(video_path):
 	frames = frames[:count]
 	frames = frames / 255
 	return frames
+
+## write out video frames to a file, specified by path
+def write_video(frames, path, fps = 29.97):
+
+	#this codec is hard coded for .mp4 or .MP4 file extensions. Use MJPG for .avi. If there's a bug in this file, it's probably this line
+	videoWriter = cv2.VideoWriter(path,cv2.VideoWriter_fourcc(*'mp4v'), fps, (frames[0].shape[1],frames[0].shape[0]))
+	if videoWriter is None:
+		return
+	
+	for frame in frames:
+		videoWriter.write((frame * 255).astype('uint8')) #needs to be converted to uint8 for the codec
+	
+	if videoWriter is not None:
+		videoWriter.release()
